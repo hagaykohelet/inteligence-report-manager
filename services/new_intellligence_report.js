5536a12d14a042aa0c892cd8fcba29923603f962
hagay kohelet <hagay2506@gmail.com>
2025-12-03 12:09:21 +0200

    delete and search funcs

--- services/new_intellligence_report.js
+++ services/new_intellligence_report.js
@@ -17,7 +17,7 @@ function save_report_to_db(report){
 }
 
 
-function get_reports(database){
+function get_reports(){
 { 
     database.sort((id1, id2)=> id1.id.localeCompare(id2.id))
     return database
@@ -25,6 +25,35 @@ function get_reports(database){
 }
 
 
-function searching_report_id(database, id){
+function searching_report_id(id){
+    for(let report of database){
+        if (report.id == id){
+            return report
+        }
+    }throw new Error("this report id not defined")
+    }
+
+
+    function delete_report_by_id(id){
+    for (let index = 0 ; index < database.length; index++){
+        if (database[index].id == id){
+            database.splice(index,1)
+            break
+        }
+    // }throw new Error("thid report id not defined")
+}}
+
     
-}
+
+
+
+
+// let report1 = create_report("3",["knife"],"adsf")
+// let report2 = create_report("1",["knife"],"adsf")
+
+// save_report_to_db(report1)
+// save_report_to_db(report2)
+// let reports = get_reports()
+// let search = searching_report_id("3")
+// let del  = delete_report_by_id("1")
+// console.log(reports);
