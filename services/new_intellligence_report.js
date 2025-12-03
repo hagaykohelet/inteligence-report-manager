d04259d22656bc21c744ce8d9e5a00763b98fae5
hagay kohelet <hagay2506@gmail.com>
2025-12-03 13:09:10 +0200

    update function

--- services/new_intellligence_report.js
+++ services/new_intellligence_report.js
@@ -40,20 +40,46 @@ function searching_report_id(id){
             database.splice(index,1)
             break
         }
-    // }throw new Error("thid report id not defined")
-}}
-
-    
+    }throw new Error("thid report id not defined")
+}
 
 
 
 
-// let report1 = create_report("3",["knife"],"adsf")
-// let report2 = create_report("1",["knife"],"adsf")
 
-// save_report_to_db(report1)
-// save_report_to_db(report2)
-// let reports = get_reports()
-// let search = searching_report_id("3")
-// let del  = delete_report_by_id("1")
+
+function updates_report(id, obj){
+    for (let key of Object.keys(obj)){
+        if (!(key in database[0])){
+            throw new Error("keys not found")
+        }
+    }
+    for (let index =0; index <database.length;index ++ ){
+        if(database[index].id === id){
+            for(let key of Object.keys(obj)){
+                database[index][key] = obj[key]
+            }
+        return  
+        }
+    }
+    throw new Error("id not found!")
+
+}
+
+
+
+
+
+let report1 = create_report("3",["knife"],"adsf")
+let report2 = create_report("1",["knife"],"adsf")
+
+save_report_to_db(report1)
+save_report_to_db(report2)
+let reports = get_reports()
+// let search = searching_report_id("5")
+// let del  = delete_report_by_id("6")
 // console.log(reports);
+let d  = {terrorist_name:"yoyo"}
+
+updates_report("1",d)
+console.log(reports);
